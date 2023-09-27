e05469e9373195e083a05d382e804117956de130
Chummy-Guy <mattaisgd@gmail.com>
2023-09-27 15:12:42 -0400

    Updated Character Controller
    Fixed (mostly) camera bug where it zooms in, Added some more weight to the character controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Security;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -11,7 +13,11 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private InputActionReference jumpControl;
     [SerializeField]
-    private float playerSpeed = 2.0f;
+    private InputActionReference runControl;
+    [SerializeField]
+    private float playerWalkSpeed = 8.5f;
+    [SerializeField]
+    private float playerRunSpeed = 12;
     [SerializeField]
     private float jumpHeight = 1.0f;
     [SerializeField]
@@ -19,6 +25,21 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private float rotationSpeed = 4;
 
+    //how long does it take to get from 0 to maximum speed
+    [SerializeField]
+    private float timeToWalkSpeed = 0.15f;
+    [SerializeField]
+    private float timeToZero = 0.05f;
+    [SerializeField]
+    private float timeToRunSpeed = 0.2f;
+    private Vector3 moveDir;
+
+    //local speed value
+    [SerializeField]
+    private float currentSpeed;
+
+    private bool running = false;
+   
     private CharacterController controller;
     private Vector3 playerVelocity;
     private bool groundedPlayer;
@@ -29,12 +50,14 @@ public class PlayerController : MonoBehaviour
     {
         movementControl.action.Enable();
         jumpControl.action.Enable();
+        runControl.action.Enable();
     }
 
     private void OnDisable()
     {
         movementControl.action.Disable();
         jumpControl.action.Disable();
+        runControl.action.Disable();
     }
 
 
@@ -42,17 +65,19 @@ public class PlayerController : MonoBehaviour
     {
         controller = gameObject.GetComponent<CharacterController>();
         player1Cam = Camera.main.transform;
+        moveDir = Vector3.zero;
     }
 
     void Update()
     {
         Move();
         Jump();
+        GroundDetect();
     }
 
     private void FixedUpdate()
     {
-        GroundDetect();
+        
         Rotate();
     }
 
@@ -78,14 +103,66 @@ public class PlayerController : MonoBehaviour
         move = player1Cam.forward * move.z + player1Cam.right * move.x;
         move.y = 0f;
 
-        controller.Move(move * Time.deltaTime * playerSpeed);
-
-        //move function
-        if (move != Vector3.zero)
+        if (runControl.action.ReadValue<float>() == 1)
         {
-            gameObject.transform.forward = move;
+            running = true;
         }
+        else
+        {
+            running = false;
+        }
+
+        //calculate the current move speed based on if running or not
+        if (move.x != 0 && move.z != 0)
+        {
+            if (running)
+            {
+                currentSpeed += ((playerRunSpeed - 0) / timeToRunSpeed) * Time.deltaTime;
+
+                //if we are running from idle give us a speed boost to at least half of walking speed to feel more snappy
+                if (currentSpeed < playerWalkSpeed / 2)
+                {
+                    currentSpeed = playerWalkSpeed;
+                }
+            }
+            else if (currentSpeed < playerWalkSpeed)
+            {
+                currentSpeed += ((playerWalkSpeed - 0) / timeToWalkSpeed) * Time.deltaTime;
+            }
+        }
+        else if (currentSpeed > 0) 
+        {
+            //bring down speed back to 0 with a little delay to give some weight to the character
+            currentSpeed += ((0 - playerWalkSpeed) / timeToZero) * Time.deltaTime; 
+        }
+
+        //clamp values to maximum speed values
+        if (running)
+        {
+            currentSpeed = Mathf.Clamp(currentSpeed, 0, playerRunSpeed);
+        }
+        else
+        {
+            //if our current speed is more than our walk speed and we let go of running
+            //slow down a little gradually back to walk for weight
+            if (currentSpeed >= playerRunSpeed)
+            {
+                currentSpeed += ((0 - playerWalkSpeed) / 0.6f) * Time.deltaTime;
+                currentSpeed = Mathf.Clamp(currentSpeed, playerWalkSpeed, playerRunSpeed);
+            }
+            else
+            {
+                currentSpeed = Mathf.Clamp(currentSpeed, 0, playerWalkSpeed);
+            }
+           
+        }
+
+
+        controller.Move(transform.forward * Time.deltaTime * currentSpeed);
+
+
     }
+
 
     #endregion
 
