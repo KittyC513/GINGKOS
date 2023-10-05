b8919edaa75519240872b51f28a003c5fa49fd59
KittyC513 <82276776+KittyC513@users.noreply.github.com>
2023-10-05 15:37:36 -0400

    Create multiple-player

---
+++ Assets/TestCube.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TestCube : MonoBehaviour
+{
+    Vector2 i_movement;
+    float moveSpeed = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        Vector3 movement = new Vector3(i_movement.x, 0, i_movement.y) * moveSpeed * Time.deltaTime;
+        transform.Translate(movement);
+    }
+
+    private void OnMove(InputValue value)
+    {
+        i_movement = value.Get<Vector2>();
+        Debug.Log("Moving");
+    }
+    private void OnMoveDown()
+    {
+        transform.Translate(transform.up);
+        Debug.Log("Moving");
+    }
+    private void OnMoveUp()
+    {
+        transform.Translate(-transform.up);
+        Debug.Log("Moving");
+    }
+
+}
